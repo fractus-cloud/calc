b11113be62f83ef125c5b837f7e5af150a7620e9
Dillon J. Newell <dillon@newell-labs.ca>
2025-09-13 01:09:30 -0400

    Update favicon

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -8,7 +8,9 @@ export default createHandler(() => (
         <head>
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link rel="icon" href="/favicon.ico" />
+          <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+          <meta name="theme-color" content="#0f172a" media="(prefers-color-scheme: dark)" />
+          <meta name="theme-color" content="#f8fafc" media="(prefers-color-scheme: light)" />
           {assets}
         </head>
         <body>
